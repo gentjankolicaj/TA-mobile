866cd7d3171d0c56f243ce9f7587729bc43b9e03
gentjan kolicaj <gentjankolicaj@gmail.com>
2019-04-01 00:16:19 +0200

    Some device test samples.

---
+++ TA-mobile/src/main/java/Tests/Android/LoginTest.java
@@ -0,0 +1,11 @@
+package Tests.Android;
+
+import com.ta.mobile.test.TestngTestCase;
+
+public class LoginTest extends TestngTestCase {
+	
+	public void run() throws Exception{
+		
+	}
+
+}
---
+++ TA-mobile/src/main/java/Tests/Windows/LoginTest.java
@@ -0,0 +1,10 @@
+package Tests.Windows;
+
+import com.ta.mobile.test.TestngTestCase;
+
+public class LoginTest  extends TestngTestCase{
+
+	public void run() throws Exception{
+		
+	}
+}
